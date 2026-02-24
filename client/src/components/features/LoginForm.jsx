7e546b46ea2cacf6f8590110fa55b3239b3d83e3
chju-orif <julien.chollet@formation.orif.ch>
2026-02-24 16:42:32 +0100

    last update client

--- client/src/components/features/LoginForm.jsx
+++ client/src/components/features/LoginForm.jsx
@@ -27,7 +27,7 @@ function LoginForm({ }) {
 
         try {
             const url = isRegistering ? '/api/register' : '/api/login';
-            const response = await fetch('http://localhost:3000' + url, {
+            const response = await fetch('https://pixeltogether.onrender.com' + url, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ pseudo, password })
