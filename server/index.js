6949dfdf42545ec2af6c564b2c0b36ad49d94cbd
Dokk <31633839+Quorak21@users.noreply.github.com>
2026-04-22 16:56:51 +0200

    Fix formatting of allowedOrigins array in index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  process.env.FRONTEND_URL
+  process.env.FRONTEND_URL,
   process.env.FRONTEND_URL2
 ].filter(Boolean);
 
